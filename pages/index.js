b01737816c2d77bb071362128a8a9bfb55744b5e
Juan Ignacio <juanchoperez.10@hotmail.com>
2019-07-12 15:37:17 -0500

    base structure

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from './components/MyLayout'
+
+export default function Index() {
+  return (
+    <Layout>
+      <div>
+        <p>Css Conf CO</p>
+      </div>
+    </Layout>
+  )
+}
